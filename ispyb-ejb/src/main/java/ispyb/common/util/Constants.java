27a245ae453dbadf91985f6b0fcfe2f47b245100
Ingvord <ingvord.mail@gmail.com>
2024-05-14 21:13:25 +0200

    Fix properties loading from classpath

--- ispyb-ejb/src/main/java/ispyb/common/util/Constants.java
+++ ispyb-ejb/src/main/java/ispyb/common/util/Constants.java
@@ -31,9 +31,9 @@ import java.util.Properties;
 public final class Constants {
 
 	public static final String ISPYB_PROPERTIES="ispyb.properties";
-	public static final String CLASSPATH_ISPYB_PROPERTIES="classpath://ISPyB.properties";
+	public static final String CLASSPATH_ISPYB_PROPERTIES="classpath:ISPyB.properties";
 
-	private static Properties mProp = PropertyLoader.loadProperties(System.getProperty(Constants.ISPYB_PROPERTIES));
+	private static Properties mProp = PropertyLoader.loadProperties(System.getProperty(Constants.ISPYB_PROPERTIES, CLASSPATH_ISPYB_PROPERTIES));
 
 	public enum SITE {
 		DESY, LOCAL
